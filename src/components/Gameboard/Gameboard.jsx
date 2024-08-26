5d70ea054042aa4b1325a51edf4a6c702ca140b5
matthewmogan <mattmogan@gmail.com>
2024-08-26 22:50:31 +0300

    added guess evaluation calculation and instructions page

--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import "./Gameboard.css"; 
 import {getRandomWord} from "../../utilities/api"
 import {createBoardState} from "../../utilities/helpers"
@@ -6,48 +6,77 @@ import GuessRow from "../GuessRow/GuessRow"
 
 export default function Gameboard(){
     
-    const startingGuesses = 6 // default starting guesses (rows)
-    const startingLetters = 5 // default starting letters (columns)
-    const boardEvals = { // object containing board evaluation key / value pairs 
-        inactive: "IA", 
-        rightLetter_RightPlace: "RR",
-        rightLetter_WrongPlace: "RW",
-        wrongLetter_WrongPlace: "WW"
+    // default starting guesses (rows) and letters (columnns):
+    const startingGuesses = 6 
+    const startingLetters = 5
+    // object containing board evaluation key / value pairs: 
+    const letterEvals = {  
+        inactive: "inactive", 
+        rightLetter_RightPlace: "rightLetter_RightPlace",
+        rightLetter_WrongPlace: "rightLetter_WrongPlace",
+        wrongLetter_WrongPlace: "wrongLetter_WrongPlace"
     }  
+    // round status state values:
+    const roundStatuses = {
+        active: "active",
+        won: "won",
+        lost: "lost"
+    }
+    const roundStatusMessages = {
+        active: "Guess the secret word!",
+        won: "You won!",
+        lost: "You lost!"
+    }
 
-    // Board dimension states - sets up the Gameboard construction:
 
-    const [keywordLetters, setKeywordLetters] = useState(startingLetters) // letters in the answer
-    const [maxGuesses, setMaxGuesses] = useState(startingGuesses) // max attempts
-    const [boardLetters, setBoardLetters] = useState(createBoardState(keywordLetters,maxGuesses,"")) // running tally of the guesses across the whole round
-    const [boardEvaluated, setBoardEvaluated] = useState(createBoardState(keywordLetters,maxGuesses,boardEvals.inactive)) 
-    const [keyword, setKeyword] = useState(getRandomWord(keywordLetters).toUpperCase()) // the answer
-    const [guessNumber, setGuessNumber] = useState(0) // attempt number
-    const [guess, setGuess] = useState("") // active guess 
+    // WordleFlex States:
 
-    // keywordLetters update event handler:
+    // letters in the answer:
+    const [keywordLetters, setKeywordLetters] = useState(startingLetters) 
+    // max guess attempts per round:
+    const [maxGuesses, setMaxGuesses] = useState(startingGuesses)
+    // 2D array representing the guessed letters:
+    const [boardLetters, setBoardLetters] = useState(createBoardState(keywordLetters,maxGuesses,"")) 
+    // 2D array representing the evaluated guesses:' 
+    const [boardEvaluated, setBoardEvaluated] = useState(createBoardState(keywordLetters,maxGuesses,letterEvals.inactive)) 
+    // the answer:
+    const [keyword, setKeyword] = useState(getRandomWord(keywordLetters).toUpperCase()) 
+    // attempt number:
+    const [guessNumber, setGuessNumber] = useState(0) 
+    // active guess: 
+    const [guess, setGuess] = useState("") 
+    // tracks whether the round is in progress, won, or lost:
+    const [roundStatus, setRoundStatus] = useState(roundStatuses.active)
+    // text to display how many guesses the player has left, and if they won or lost
+    const [statusText, setStatusText] = useState(roundStatusMessages.active)
+    
+    // keywordLetters update event handler - gets a new keyword of a certain length, resets the board and restarts the game
 
     const updateKeywordLetters = (event) => {
         event.preventDefault()
         setKeywordLetters(Number(event.target.value))
         setKeyword(getRandomWord(Number(event.target.value)).toUpperCase())
         setBoardLetters(createBoardState(Number(event.target.value),maxGuesses,""))
-        setBoardEvaluated(createBoardState(Number(event.target.value),maxGuesses,boardEvals.inactive))
+        setBoardEvaluated(createBoardState(Number(event.target.value),maxGuesses,letterEvals.inactive))
         setGuessNumber(0)
+        setRoundStatus(roundStatuses.active)
+        setStatusText(roundStatusMessages.active)
     }
 
-    // maxGuesses update event handler:
+    // maxGuesses update event handler - adds or subtracts guess rows from the board. resets the board and restarts the game
 
     const updateMaxGuesses = (event) => {
         event.preventDefault()
-        setMaxGuesses(Number(event.target.value)) // reset max guesses
+        setMaxGuesses(Number(event.target.value)) 
         setKeyword(getRandomWord(Number(event.target.value)).toUpperCase())
-        setBoardLetters(createBoardState(keywordLetters,Number(event.target.value),"")) // reset boardstate
-        setBoardEvaluated(createBoardState(keywordLetters,Number(event.target.value),boardEvals.inactive))
-        setGuessNumber(0) // reset guess number
+        setBoardLetters(createBoardState(keywordLetters,Number(event.target.value),""))
+        setBoardEvaluated(createBoardState(keywordLetters,Number(event.target.value),letterEvals.inactive))
+        setGuessNumber(0) 
+        setRoundStatus(roundStatuses.active)        
+        setStatusText(roundStatusMessages.active)
     }
 
-    // updateGuess event handler:
+    // updateGuess event handler - capitalizes the user guess and logs the guess in state
 
     const updateGuess = (event) => {
         event.preventDefault()
@@ -56,43 +85,67 @@ export default function Gameboard(){
         setGuess(string)
     }
 
+    // Calculate Guess 
+
     const calculateGuess = (event) => {
         
         event.preventDefault()
         
-        if(guessNumber === maxGuesses){
-            return // exit if user is out of guesses
+        // 1. Exit if the user is out of guesses, or round state is not active
+        
+        if(guessNumber === maxGuesses || roundStatus !== roundStatuses.active ){
+            return 
         }
-        
-        // 1. Update the letter state of the board 
-        
-        const guessArray = [...guess] // spread the guess into an Array
-        const newBoardLetters = boardLetters.map(row => [...row]) // create a deep copy of the 2D state, so we don't update state directly
-        newBoardLetters[guessNumber] = guessArray // update the copy of the board state with the guess inserted       
-        setBoardLetters(newBoardLetters) // update the letter state of the board
 
-        // 2. Evaluate the guess and update the evaluated state
+        // 2. Update the letter state of the board 
+        
+        // spread the guess into an Array:
+        const guessArray = [...guess] 
+        // create a deep copy of the 2D state, so we don't update state directly:
+        const newBoardLetters = boardLetters.map(row => [...row]) 
+        // update the copy of the board state with the guess inserted:
+        newBoardLetters[guessNumber] = guessArray 
+        // update the letter state of the board:
+        setBoardLetters(newBoardLetters) 
+
+        // 3. Evaluate the guess and update the evaluated state and guess number
         
         const evaluatedArray = Array(keywordLetters)
         const keywordArray = Array.from(keyword)
         const newBoardEvaluated = boardEvaluated.map(row => [...row])
-
         guessArray.forEach((x,index) => {
             if (x === keywordArray[index]) {
-                evaluatedArray[index] = boardEvals.rightLetter_RightPlace
+                evaluatedArray[index] = letterEvals.rightLetter_RightPlace
             } else if (keywordArray.some(y => x === y)) {
-                evaluatedArray[index] = boardEvals.rightLetter_WrongPlace
+                evaluatedArray[index] = letterEvals.rightLetter_WrongPlace
             } else 
-               evaluatedArray[index] = boardEvals.wrongLetter_WrongPlace         
+               evaluatedArray[index] = letterEvals.wrongLetter_WrongPlace         
         })
+        newBoardEvaluated[guessNumber] = [...evaluatedArray] 
+        setBoardEvaluated(newBoardEvaluated) 
+        setGuessNumber(guessNumber+1) 
 
-        newBoardEvaluated[guessNumber] = [...evaluatedArray] // update the 
-        setBoardEvaluated(newBoardEvaluated) // update the evaluated state
+        // 4. Check if the player won or lost and update the round status and status text
 
-        // 3. Update other relevant states
+        if(evaluatedArray.every((x) => x === letterEvals.rightLetter_RightPlace)){
+            setRoundStatus(roundStatuses.won) 
+            setStatusText(updateStatusText(roundStatuses.won))
+        } else if (guessNumber+1 === maxGuesses) {
+            setRoundStatus(roundStatus.lost, guessNumber+1)
+            setStatusText(updateStatusText(roundStatuses.lost))
+        }
 
-        setGuessNumber(guessNumber+1) // update the guess number
+    }
 
+    const updateStatusText = (status) => {
+        switch(status) {
+            case roundStatuses.active:
+                return "Guess the word!"
+            case roundStatuses.won:
+                return "You won!"
+            case roundStatuses.lost:
+                return "You lost!"
+        }
     }
 
     return (
@@ -123,7 +176,7 @@ export default function Gameboard(){
                 <GuessRow
                     key = {arg}
                     id = {arg} // tell GuessRow which ROW it is
-                    keywordLetters = {keywordLetters} // tell GuessRow how many letters (COLs) in the keyword
+                    keywordLetters = {keywordLetters} // tell GuessRow how many letters (COL) in the keyword
                     boardLetters = {boardLetters} // Allow GuessRow to pass the Letters their value
                     boardEvaluated = {boardEvaluated} // Allow GuessRow to pass the Letters their evaluation class
                 />
@@ -143,6 +196,9 @@ export default function Gameboard(){
                 value="Guess"
             />
         </form>
+        <div className="statusInfo">
+           <h2>{statusText}</h2>
+        </div>
         </>
     )
 }
--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -3,8 +3,21 @@ import "./Instructions.css";
 
 export default function Instructions(){
     return (
-        <>
-            <h1>Instructions</h1>
-        </>
+        <div className="instructions">
+            <h1 className= "instructionsH1">How to Play WordleFlex</h1>
+            <p className="instructionsP">Welcome to WordleFlex. In this game, you try to guess the secret word before your run out of guesses!</p>
+            <ul>
+                <li><strong>Green:</strong> The letter is correct and in the right position.</li>
+                <li><strong>Yellow:</strong> The letter is correct but in the wrong position.</li>
+                <li><strong>Gray:</strong> The letter is not in the word at all.</li>
+            </ul>
+            <p className="instructionsP">Use the clues to guess the word within your specific number of attempts.</p>
+            <h1 className= "instructionsH1">WordleFlex Settings</h1>
+            <p className="instructionsP">Our twist of the classic Wordle includes the options to set your own number of guesses and letters, increasing or decreasing the difficulty!</p>
+            <ul>
+                <li><strong>Guesses:</strong> Changes number of guesses per round</li>
+                <li><strong>Letters:</strong> Changes the number of letters in the secret word</li>
+            </ul>
+        </div>
     )
 }
