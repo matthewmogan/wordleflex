9adb093157034ea5580bfa66879f8ea7311e1cec
matthewmogan <mattmogan@gmail.com>
2024-08-27 15:34:54 +0300

    fixed browser router routing to start on game page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,26 +6,26 @@ import Instructions from '../Instructions/Instructions'
 function App() {
   return (
     <>
-    <nav>
-      <ul>
-        <li>
-          <NavLink 
-            to="/game"
-          >WordleFlex
-          </NavLink>
-        </li>
-        <li>
-          <NavLink 
-            to="/instructions"
-x          >Instructions
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path ="/game" element={<Gameboard/>}/>
-      <Route path ="/instructions" element ={<Instructions/>}/>
-    </Routes>
+      <nav>
+        <ul>
+          <li>
+            <NavLink 
+              to="/wordleflex"
+            >WordleFlex
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/instructions"
+  x          >Instructions
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path ="/wordleflex" element={<Gameboard/>}/>
+        <Route path ="/instructions" element ={<Instructions/>}/>
+      </Routes>
     </>
   );
 }
